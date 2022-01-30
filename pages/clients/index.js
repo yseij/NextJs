d4621ff4ec6f3aefd7cdbcec18f21723bcbbb9e2
yseij <r0790156@student.thomasmore.be>
2022-01-30 18:21:38 +0100

    navigatie naar dynamische routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,23 @@
-function ClientsPage(){
-    return (
-        <div>
-            <h1>The Clients Page</h1>
-        </div>
-    )
+import Link from "next/link";
+
+function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ClientsPage;
